737a09edbd21bf13e47c3520284d78bb4ed3f63a
Mohit Sehgal <mohit@relifems.com>
2024-05-11 22:10:32 +0530

    consumer

---
+++ consumer.js
@@ -0,0 +1,26 @@
+let dotenv = require("dotenv")
+dotenv.config()
+
+const { Kafka } = require('kafkajs')
+
+const kafka = new Kafka({
+  clientId: 'demo-app',
+  brokers: ['localhost:9092', 'localhost:9092'],
+});
+
+
+let main = async function(){
+    const consumer = kafka.consumer({ groupId: 'test-group' })
+
+    await consumer.connect()
+    await consumer.subscribe({ topic: 'quickstart-events', fromBeginning: true })
+    
+    await consumer.run({
+      eachMessage: async ({ topic, partition, message }) => {
+        console.log({
+          value: message.value.toString(),
+        })
+      },
+    })
+};
+main();
